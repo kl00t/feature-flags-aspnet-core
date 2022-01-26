4780ebdb24b9b6ac170148cef9a93fd5e97d4fa3
scottvaughanamido <scott.vaughan@amido.com>
2022-01-26 09:44:23 +0000

    Add feature management code

---
+++ ProCodeGuide.Samples.FeatureFlagsDemo/Models/Mobile.cs
@@ -0,0 +1,13 @@
+﻿namespace ProCodeGuide.Samples.FeatureFlagsDemo.Models
+{
+    public class Mobile
+    {
+        public string? Id { set; get; }
+
+        public string? Name { set; get; }
+
+        public string? Specfication { set; get; }
+
+        public MobileReview? ReviewDetails { set; get; }
+    }
+}
---
+++ ProCodeGuide.Samples.FeatureFlagsDemo/Models/MobileReview.cs
@@ -0,0 +1,9 @@
+﻿namespace ProCodeGuide.Samples.FeatureFlagsDemo.Models
+{
+    public class MobileReview
+    {
+        public string? Rating { set; get; }
+
+        public string? Remarks { set; get; }
+    }
+}
